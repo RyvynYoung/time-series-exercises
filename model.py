d2b7433a3aec223345576e8288418161d973df42
RyvynYoung <ryvyn.young@gmail.com>
2020-11-02 19:53:13 -0600

    model.py functions added

--- model.py
+++ model.py
@@ -15,6 +15,8 @@ from statsmodels.tsa.api import Holt
 import warnings
 warnings.filterwarnings("ignore")
 
+import prepare
+
 ######## time series #######
 def split_data(df):
     '''splits into train, validate, test'''
@@ -54,12 +56,12 @@ def chart_splits(train, validate, test):
         plt.title(col)
         plt.show()
 
-def evaluate(target_var):
+def evaluate(target_var, validate, predictions):
     '''evaluate() will compute the Mean Squared Error and the Rood Mean Squared Error to evaluate'''
-    rmse = round(sqrt(mean_squared_error(validate[target_var], yhat_df[target_var])), 0)
+    rmse = round(sqrt(mean_squared_error(validate[target_var], predictions[target_var])), 0)
     return rmse
 
-def plot_and_eval(target_var):
+def plot_and_eval(target_var, train, validate, predictions):
     '''
     plot_and_eval() will use the evaluate function and also plot train and test values with the predicted
      values in order to compare performance.
@@ -67,9 +69,9 @@ def plot_and_eval(target_var):
     plt.figure(figsize = (12,4))
     plt.plot(train[target_var], label='Train', linewidth=1)
     plt.plot(validate[target_var], label='Validate', linewidth=1)
-    plt.plot(yhat_df[target_var])
+    plt.plot(predictions[target_var])
     plt.title(target_var)
-    rmse = evaluate(target_var)
+    rmse = evaluate(target_var, validate, predictions)
     print(target_var, '-- RMSE: {:.0f}'.format(rmse))
     plt.show()
 
@@ -79,9 +81,54 @@ def create_eval_df():
     return eval_df
 
 # function to store the rmse so that we can compare, note: need to run create_eval_df before this function
-def append_eval_df(model_type, target_var):
-    rmse = evaluate(target_var)
+def append_eval_df(eval_df, validate, predictions, model_type, target_var):
+    rmse = evaluate(target_var, validate, predictions)
     d = {'model_type': [model_type], 'target_var': [target_var],
         'rmse': [rmse]}
     d = pd.DataFrame(d)
     return eval_df.append(d, ignore_index = True)
+
+
+def last_observed_predictions(train, validate):
+    '''get last observed value and set that as prediction for all in validate'''
+    consumption = train['Consumption'][-1:][0]
+    wind = train['Wind'][-1:][0]
+    solar = train['Solar'][-1:][0]
+    calc_windsolar = train['calc_windsolar'][-1:][0]
+    # get preditions
+    yhat_df = pd.DataFrame({'Consumption': [consumption], 'Wind': [wind], 'Solar': [solar], 
+                            'calc_windsolar': [calc_windsolar]}, index = validate.index)
+    return yhat_df
+
+def plot_pred_values(train, validate, predictions):
+    # plot predicted values
+    for col in train.columns:
+        plot_and_eval(col, train, validate, predictions)
+
+
+# create function for later repitition
+def make_predictions(consumption, wind, solar, calc_windsolar, validate):
+    yhat_df = pd.DataFrame({'Consumption': [consumption], 'Wind': [wind], 'Solar': [solar], 
+                            'calc_windsolar': [calc_windsolar]}, index = validate.index)
+    return yhat_df
+
+def simple_average_predictions(train, validate):
+    # get average and use that to make predictions for all in validate
+    consumption = train['Consumption'].mean()
+    wind = train['Wind'].mean()
+    solar = train['Solar'].mean()
+    calc_windsolar = train['calc_windsolar'].mean()
+    yhat_df = make_predictions(consumption, wind, solar, calc_windsolar, validate)
+    return yhat_df
+
+def rolling_avg_pred(train, validate, period):
+    # compute rolling average, 
+    period = period
+    consumption = round(train['Consumption'].rolling(period).mean().iloc[-1], 1)
+    wind = round(train['Wind'].rolling(period).mean().iloc[-1], 1)
+    solar = round(train['Solar'].rolling(period).mean().iloc[-1], 1)
+    calc_windsolar = round(train['calc_windsolar'].rolling(period).mean().iloc[-1], 1)
+
+    yhat_df = make_predictions(consumption, wind, solar, calc_windsolar, validate)
+    return yhat_df
+
